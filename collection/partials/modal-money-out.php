588165a34f02011540c92cda31e4f6184ca932e4
Bryan Asnan <bryan.asnan29@gmail.com>
2014-11-12 15:12:11 +0800

    [FORDEPLOY] MN: Money out mockup

--- collection/partials/modal-money-out.php
+++ collection/partials/modal-money-out.php
@@ -4,26 +4,32 @@
     <div class="modal-content">
 	<div class="modal-header">
 	 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-         <h2 id="captionPayment">Money Out</h2>
+         <h2 id="captionPayment">Enter Your Expense</h2>
 	</div>
 	
 	<div class="modal-body">
 
 		<div class="control-group">
-			<label class="control-label" for="dateRecieved">Date Received</label>
+			<label class="control-label" for="dateRecieved">Date Paid</label>
 			<div class="controls">
 				<input autocomplete="off" data-is-required="true" name="dateRecieved" id="dateRecieved" placeholder="01-Jan-2014"s class="ui-date-received enable-click-date-window form-control" maxlength="30" tabindex="1" type="text" value="">
 			</div>
 		</div>	
 
         <div class="control-group">
-            <label class="control-label" for="fortype">Received For</label>
+            <label class="control-label" for="fortype">Paid For</label>
             <div class="controls">
-               	<select class="k-dd" >					
-					<option value="1">Bond</option>
-					<option value="2">Rent</option>
-					<option value="3">Water</option>
-					<option value="4">Bills</option>
+               	<select class="k-dd" >	
+              <option value="1">General Maintenance</option>
+              <option value="2">Electrical</option>
+              <option value="3">Plumbing</option>
+              <option value="4">Property Rates</option>
+              <option value="5">Building Insurance</option>
+              <option value="6">Landlord Insurance</option>
+              <option value="7">Cleaning</option>
+              <option value="8">Gardening</option>
+              <option value="9">Building</option>
+              <option value="10">Painting</option>		
 					<option value="">Add more type...</option>
 				</select>
             </div> 
@@ -33,6 +39,13 @@
             <div class="controls">
                 <input autocomplete="off" data-is-required="true" name="newtype" id="newtype" placeholder="Enter New Type" class="form-control" maxlength="30" tabindex="1" value="" type="text" >
             </div>
+        </div>
+
+                <div class="control-group" id="section_description">
+            <label class="control-label" for="description">Description</label>
+            <div class="controls">
+                <textarea name="description" id="description" placeholder="Repair dishwasher handle" class="form-control"  maxlength="1000"></textarea>  
+            </div> 
         </div>
 
 
