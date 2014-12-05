8aed9174005a721c0a02783caf7004be677c13e9
Felix <felix@rentmyestate.com.au>
2014-12-05 12:49:43 +0800

    [FORDEPLOY] MN: stripe webhook

---
+++ stripe/index.php
@@ -0,0 +1,34 @@
+<?php
+   // You can get your webhook endpoint from your Slack settings
+
+require_once('lib/Stripe.php'); 
+Stripe::setApiKey("sk_test_4ScS1JhfEBAMD47lROwZcxXN");
+
+$input = @file_get_contents("php://input");
+$message = json_decode($input);
+
+
+      $ch = curl_init();
+      $url = "https://myestate.slack.com/services/hooks/slackbot?token=3YzHDutHK2ux69X0mDuZDbSa&channel=%23system-message";
+
+      $breaks = array("<br />","<br>","<br/>");  
+      $message = str_ireplace($breaks, "\r\n", $message); 
+      $message = '```'.$message.'```';
+
+      curl_setopt($ch, CURLOPT_URL,$url);
+      curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $message);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+      $result = curl_exec($ch);
+
+/**
+      if(curl_errno($ch)){
+        echo 'error:' . curl_error($ch);
+      }
+      **/
+      curl_close($ch);       
+      // var_dump($result);
+
+ ?>
