7e55b2878503edac0b065a0c7e224534a8d73c88
Bryan Asnan <bryan.asnan29@gmail.com>
2015-02-10 03:21:27 +0800

    RME-1005

--- partials/bread.php
+++ partials/bread.php
@@ -1,17 +1,15 @@
 <section class="subnavbar">
-	<ul class="ui-bread clearfix gd">
-									<li class="dropdown ui-crumbs start">
-											<a href="#" data-toggle="dropdown" role="button" class="dropdown-toggle ui-c-item hv-fa" id="propertyList">
-	<span class="fa fa-chevron-down"></span> My Properties
-</a>
-
-
-									</li>
-												<li class="ui-crumbs trail">
-											<a href="https://members-dev.rentmyestate.com.au/dashboard/switcher/9" class="ui-c-item">123/2 Sample, Wehla  3518</a>
-									</li>
-												<li class="ui-crumbs end">
-											<span class="ui-c-item">Record Rent Payments</span>
-									</li>
-						</ul>
+    <ul class="ui-bread clearfix gd">
+        <li class="dropdown ui-crumbs start">
+            <a href="#" data-toggle="dropdown" role="button" class="dropdown-toggle ui-c-item hv-fa" id="propertyList">
+                <span class="fa fa-chevron-down"></span> My Properties
+            </a>
+        </li>
+        <li class="ui-crumbs trail">
+            <a href="https://members-dev.rentmyestate.com.au/dashboard/switcher/9" class="ui-c-item">123/2 Sample, Wehla  3518</a>
+        </li>
+        <li class="ui-crumbs end">
+            <span class="ui-c-item">Current Page</span>
+        </li>
+    </ul>
 </section>
