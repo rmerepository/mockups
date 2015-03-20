b350345f9d548f2d5afc1ec6113128ed41b46fc1
Bryan Asnan <bryan.asnan29@gmail.com>
2015-03-20 15:09:02 +0800

    [FORDEPLOY] MN: Tenant Dashboard

--- dashboard/tenant-partials/lease-snapshot.php
+++ dashboard/tenant-partials/lease-snapshot.php
@@ -1,5 +1,21 @@
 <div style="max-width: 700px; margin:auto">
 <h3 class="mtop-none">Snapshot of your lease</h3>
+<div>
+	<div class="property-image-w" style="background-image: url('https://members.rentmyestate.com.au/assets/uploads/userfiles/1263/1263-1018058029530fe3971339d')">
+	</div>
+	
+	<div class="row" style="margin-top:10px;">
+		<div class="col-xs-3">
+			<span class="ipd df-icons-bedrooms"></span> &nbsp;&nbsp; <span class="item-qty">4</span>
+		</div>
+		<div class="col-xs-3">
+			<span class="ipd df-icons-bathroom"></span> &nbsp;&nbsp; <span class="item-qty">1</span>
+		</div>
+		<div class="col-xs-3">
+			<span class="ipd df-icons-car-space"></span> &nbsp;&nbsp; <span class="item-qty">2</span>
+		</div>
+	</div>
+</div>
 <div class="info-block">
 <div class="row me-well"> 
 			<div class="pull-left">
