33269ab88834422a02e28edc90cfef565ef03c72
Bryan Asnan <bryan.asnan29@gmail.com>
2015-04-23 13:50:31 +0800

    [FORDEPLOY] MJ: Dashboard live feed color

--- components/live-feed.php
+++ components/live-feed.php
@@ -1,28 +1,97 @@
 <div class="activity-feed">
 	<ul class="wrap-feeds">
 			<li class="activity-date"><span>Today</span></li>
-			<li class="feed first dollar">
-				<div class="activity">Next Payment Due $350</div>
+			<li class="feed first house">
+				<div class="activity">Your ad is LIVE. Make changes</div>
 				<div class="time">5 mins ago</div>
 			</li>
-			<li class="feed wrench">
-				<div class="activity">Maintenance Request received. <a href="#">View</a></div>
+
+			<li class="feed  house ">
+				<div class="activity">Property withdrawn (ad taken down)</div>
+				<div class="time">5 mins ago</div>
+			</li>
+
+			<li class="feed  house ">
+				<div class="activity">Ad expires. <a href="#">Extent now (free)</a></div>
+				<div class="time">5 mins ago</div>
+			</li>
+
+
+				<li class="feed user">
+				<div class="activity">Application received from Jack. <a href="#">View</a></div>
+				<div class="time">8 hrs ago</div>
+			</li>
+
+			<li class="feed user">
+				<div class="activity">Tom &amp; Sally’s application accepted</div>
+				<div class="time">8 hrs ago</div>
+			</li>
+
+
+				<li class="feed user-invite">
+				<div class="activity">Tenant Invitation sent to Tom &amp; Sally <a href="#">View</a></div>
+				<div class="time">8 hrs ago</div>
+			</li>
+
+			<li class="feed user-invite">
+				<div class="activity">Tom &amp; Sally is now added as a tenant</div>
 				<div class="time">8 hrs ago</div>
 			</li>
 			
-				<li class="feed pencil">
-				<div class="activity">Heads up - Routine Inspection <a href="#">Help</a></div>
+
+
+
+			
+			<li class="feed  dollar">
+				<div class="activity">Next Payment Due $350</div>
+				<div class="time">5 mins ago</div>
+			</li>
+
+			<li class="feed dollar">
+				<div class="activity">$350 rent paid by Tom &amp; Sally</div>
+				<div class="time">5 mins ago</div>
+			</li>
+
+		<li class="feed notification">
+				<div class="activity">Late rent reminder sent.<a href="#">View</a></div>
 				<div class="time">8 hrs ago</div>
 			</li>
+ 
+<!-- 			<li class="feed wrench">
+				<div class="activity">Maintenance Request received. <a href="#">View</a></div>
+				<div class="time">8 hrs ago</div>
+			</li> -->
+<!-- 
 		<li class="activity-date"><span>12 March</span></li>	
 		<li class="feed wrench">
 				<div class="activity">Maintenance Request received. <a href="#">View</a></div>
 				<div class="time">1 day ago</div>
-		</li>
+		</li> -->
 		<li class="activity-date"><span>11 March</span></li>
-			<li class="feed pencil">
-				<div class="activity">Heads up - Routine Inspection <a href="#">Help</a></div>
+			<li class="feed phone">
+				<div class="activity">Phone enquiry from Jessica. <a href="#">View</a></div>
 				<div class="time">2 days ago</div>
 			</li>
+			<li class="feed email">
+				<div class="activity">Email enquiry from Tom & Sally. <a href="#">View</a></div>
+				<div class="time">2 days ago</div>
+			</li>
+
+
+		<li class="activity-date"><span>10 March</span></li>
+			<li class="feed calendar">
+				<div class="activity">Lease start date 28 April 2015.  <a href="#">Checklist</a></div>
+				<div class="time">2 days ago</div>
+			</li>
+			<li class="feed calendar">
+				<div class="activity">Lease End Date </div>
+				<div class="time">2 days ago</div>
+			</li>
+						<li class="feed calendar">
+					<div class="activity">Heads up - Lease ends 28 April 2015. <a href="#">Be proactive</a></div>
+				<div class="time">2 days ago</div>
+			</li>
+
+
 	</ul>
 </div> 
