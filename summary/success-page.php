49a7e616f7be257d07d3816e7f981e16d13efa88
Bryan Asnan <bryan.asnan29@gmail.com>
2015-07-17 17:23:08 +0800

    [FORDEPLOY] MJ: Summary

---
+++ summary/success-page.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Rent My Estate</title>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="viewport" content="width=device-width">
+		<meta name="description" content="RME" />
+		<meta name="keywords" content="rent, realestate" />
+		<meta name="author" content="Rent My Estate" />
+		<link href='//fonts.googleapis.com/css?family=Roboto:100,400,300,500' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="/assets/css/_style_bundle2.css?v=1437117414" type='text/css'>
+		<!-- extra CSS -->
+		<link rel="stylesheet" href="/assets/css/fileupload/bootstrap-image-gallery.min.css?v=1437117414">
+		<link rel="stylesheet" href="/assets/css/fileupload/jquery.fileupload-ui.css?v=1437117414">
+		<link rel="stylesheet" href="/partials/css/modal-plan.css?v=1437117414">
+		<link href='//netdna.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css' rel='stylesheet' type='text/css'>
+		<style>
+			.success-page-wrap {
+			 margin-top: 30px;
+			}
+			.copylink {
+				max-width: 502px;
+  			margin: 55px auto 8px;
+			}
+			.button-wrap {
+				  margin-top: 45px;
+			}
+		</style>
+	</head>
+	<body>
+		<div class="success-page-wrap text-center">
+			<img src="/assets/images/c/success-banner.png" alt="Congrats!">
+			<h1>Woohoo Congrats!</h1>
+			<p class="large-text desc">Your ad will be live and ready to be seen by thousands in no time</p>
+			<div class="input-group copylink">
+				<input type="text" class="form-control" value="https://property.rentmyestate.com.au/view/3461/nsw/106-156-pacific-highway-north-sydney-2060/">
+				<span class="input-group-btn">
+				  <button class="btn btn-primary" type="button">Copy Link</button>
+				</span>
+			</div>
+			<div class="lighter-text">Share this link to potential tenants to apply for your property online</div>
+			<div class="button-wrap">
+				<a href="#" class="btn btn-light btn-mega">Return to Ads</a>
+			</div>
+		</div>
+	</body>
+</html>
